37fbb8d6fd86882ad2272dc659ed7f7d7e778ba9
Eray Keskin <eraykeskinmac@gmail.com>
2023-06-27 22:41:27 +0300

    auth page router guard

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,14 +1,27 @@
 import Navbar from "@/components/Navbar";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import AuthModal from "@/components/Modals/AuthModal";
 import { authModalState } from "@/atoms/authModalAtom";
 import { useRecoilValue } from "recoil";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/firebase";
+import { useRouter } from "next/router";
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
-  const authModal = useRecoilValue(authModalState)
+  const authModal = useRecoilValue(authModalState);
+  const [user, loading, error] = useAuthState(auth);
+  const [pageLoading, setPageLoading] = useState<boolean>(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) router.push("/");
+    if (!loading && !user) setPageLoading(false);
+  }, [user, router, loading]);
+
+  if (pageLoading) return null;
   return (
     <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
       <div className="max-w-7-xl mx-auto">
@@ -16,7 +29,7 @@ const AuthPage: React.FC<AuthPageProps> = () => {
         <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
           <Image src="/hero.png" alt="Hero Image" width={750} height={750} />
         </div>
-       {authModal.isOpen &&  <AuthModal />}
+        {authModal.isOpen && <AuthModal />}
       </div>
     </div>
   );
