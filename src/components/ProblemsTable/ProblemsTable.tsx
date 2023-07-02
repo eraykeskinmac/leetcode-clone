4407ee99ee6412c5effea893738da9963a3b51fb
Eray Keskin <eraykeskinmac@gmail.com>
2023-07-02 17:05:50 +0300

    problems table header

---
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type ProblemsTableProps = {};
+
+const ProblemsTable: React.FC<ProblemsTableProps> = () => {
+  return <div>Have a good coding</div>;
+};
+export default ProblemsTable;
