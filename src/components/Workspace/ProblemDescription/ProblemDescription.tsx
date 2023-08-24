cba9d7428dec9557ac9552078440085dd54957ea
Eray Keskin <meraykeskin@gmail.com>
2023-08-24 16:49:42 +0300

    Add styling to the problem description component

--- src/components/Workspace/ProblemDescription/ProblemDescription.tsx
+++ src/components/Workspace/ProblemDescription/ProblemDescription.tsx
@@ -3,6 +3,6 @@ import React from "react";
 type ProblemDescriptionProps = {};
 
 const ProblemDescription: React.FC<ProblemDescriptionProps> = () => {
-  return <div>Problem</div>;
+  return <div className="h-screen">Problem</div>;
 };
 export default ProblemDescription;
