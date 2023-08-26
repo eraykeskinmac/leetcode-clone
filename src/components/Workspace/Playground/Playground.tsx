4ad4acc2d5cb00cab80256f4f3176a37e37ef6c4
Eray Keskin <meraykeskin@gmail.com>
2023-08-26 22:13:57 +0300

    create code editor

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,13 +1,40 @@
 import React from "react";
 import PrefenceNav from "./PrefenceNav/PrefenceNav";
+import Split from "react-split";
+import ReactCodeMirror from "@uiw/react-codemirror";
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { javascript } from "@codemirror/lang-javascript";
 
 type PlaygroundProps = {};
 
 const Playground: React.FC<PlaygroundProps> = () => {
   return (
-    <>
+    <div className="flex flex-col bg-dark-layer-1 relative">
       <PrefenceNav />
-    </>
+
+      <Split
+        className="h-[calc(100vh-94px)]"
+        direction="vertical"
+        sizes={[60, 40]}
+        minSize={60}
+      >
+        <div className="w-full overflow-auto">
+          <ReactCodeMirror
+            value="const a = 1;"
+            theme={vscodeDark}
+            extensions={[javascript()]}
+            style={{ fontSize: 16 }}
+          />
+        </div>
+        <div className="w-full px-5 overflow-auto">
+          <div className="flex h-10 items-center space-x-6">
+            <div className="relative flex h-full flex-col justiyf-center cursor-pointer">
+                <div>Test case</div>
+            </div>
+          </div>
+        </div>
+      </Split>
+    </div>
   );
 };
 export default Playground;
